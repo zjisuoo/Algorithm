83fe57ef7838311bb3b55da2a208ff3ab79658ea
zjisuoo <moonjs96@naver.com>
2024-05-03 14:41:04 +0900

    앱

--- Python/Baekjoon/7579.py
+++ Python/Baekjoon/7579.py
@@ -1,24 +1,17 @@
 N, M = map(int, input().split())
-A = [0] + list(map(int, input().split()))
-C = [0] + list(map(int, input().split()))
+A = list(map(int, input().split()))
+C = list(map(int, input().split()))
 
-dp = [[0 for _ in range(sum(C) + 1)] for _ in range(N+1)]
-result = sum(C)
+dp = [[0 for _ in range(10000 + 1)] for _ in range(N+1)]
 
 for i in range(1, N+1) :
-	byte = A[i]
-	cost = C[i]
-
-	for j in range(1, sum(C) + 1) :
-		if j < cost :
-			dp[i][j] = dp[i-1][j]
+	for j in range(10000+1) :
+		if C[i-1] <= j :
+			dp[i][j] = max(A[i-1] + dp[i-1][j-C[i-1]], dp[i-1][j])
 		else :
-			dp[i][j] = max(byte + dp[i-1][j-cost], dp[i-1][j])
+			dp[i][j] = dp[i-1][j]
 
-		if dp[i][j] >= M :
-			result = min(result, j)
-
-if M != 0 :
-	print(result)
-else :
-	print(0)
+for i, c in enumerate(dp[-1]) :
+	if c >= M :
+		print(i)
+		break
